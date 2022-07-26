2f63d6274b54f3665c58d01eb74fa0b4abf94710
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-07-26 09:12:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -19,5 +19,36 @@ get_header();
 // if (function_exists('bcn_display')) {
 // 	bcn_display();
 // }
+?>
 
+<div class="h_urbn_main_blog_section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                <?php
+                if (have_posts()) {
+                    // Start the Loop.
+                    while (have_posts()) {
+                        the_post();
+
+                        /*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+                        get_template_part('loop-templates/content', get_post_format());
+                    }
+                } else {
+                    get_template_part('loop-templates/content', 'none');
+                }
+                ?>
+                <?php understrap_pagination(); ?>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4"></div>
+        </div>
+    </div>
+</div>
+
+
+<?php
 get_footer();
